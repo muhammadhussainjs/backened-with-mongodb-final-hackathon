46055472f151b6eedb4add27a42c87532252f28d
muhammadhussainjs <hussain356q@gmail.com>
2024-06-29 07:14:57 +0300

    port fixed and health check route added

--- index.mjs
+++ index.mjs
@@ -7,11 +7,17 @@ app.use(cors());
 
 db.connection.once('open', () => console.log("connected to db")).on("error", (err) => console.log("error connecting db -->", err))
 
-app.listen(3001, function() {
+const port = process.env.PORT || 3001
+
+app.listen(port, function () {
     console.log('Server is running at port 3001')
 })
 
 app.use(express.json())
+
+app.get("/health", (_, res) => {
+    res.send({ message: "server theek hy" })
+})
 
 //GET, POST, PUT, DELETE
 app.use('/', routes)
